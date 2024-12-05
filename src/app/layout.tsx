c0cdc43bce55418d3490b2bb89da90ab52d01b86
Javier Madariaga <145422138+ldkjvvier@users.noreply.github.com>
2024-12-04 23:12:36 -0300

    fix: Adjust layout structure in RootLayout for proper footer placement and remove unnecessary body height

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import ThemeRegistry from './components/themeRegistry'
 import './globals.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -24,11 +25,13 @@ export default function RootLayout({
 					content="initial-scale=1, width=device-width"
 				/>
 			</head>
-
 			<ThemeRegistry>
 				<body className={inter.className}>
-					<main className="tw-flex tw-min-h-screen tw-flex-col tw-items-center tw-justify-between tw-bg-transparent">
+					<main className="tw-flex tw-flex-1 tw-justify-between tw-min-h-screen tw-flex-col tw-bg-transparent">
+						<Header />
+
 						{children}
+						<Footer />
 					</main>
 				</body>
 			</ThemeRegistry>
