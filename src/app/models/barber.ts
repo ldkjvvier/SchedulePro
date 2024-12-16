f7edebca6cb7a4d174fac4cc919a17a82cf0830c
Javier Madariaga <145422138+ldkjvvier@users.noreply.github.com>
2024-12-16 19:32:56 -0300

    feat: Add Barber and Service interfaces for improved data structure

---
+++ src/app/models/barber.ts
@@ -0,0 +1,33 @@
+export interface Barber {
+	id: string
+	name: string
+	email: string
+	image: string
+	specialties: string[]
+	availability: Availability
+	bio: string
+	contactNumber: string
+}
+
+export interface Availability {
+	monday: string
+	tuesday: string
+	wednesday: string
+	thursday: string
+	friday: string
+	saturday: string
+	sunday: string
+}
+
+export interface BarberService {
+	barberId: string
+	services: Service[]
+}
+
+export interface Service {
+	id: string
+	name: string
+	price: number
+	duration: string
+	description: string
+}
