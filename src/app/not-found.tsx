82cacc68da0d984ef84e7dc7f611cc14d74937a3
Javier Madariaga <145422138+ldkjvvier@users.noreply.github.com>
2024-12-19 23:29:12 -0300

    feat: Enhance Custom404 component with customizable error message display

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,17 @@
 'use client'
-export default function Custom404() {
+
+import { Typography } from '@mui/material'
+
+export default function Custom404({
+	message = '',
+}: {
+	message: string
+}) {
 	return (
-		<>
+		<div>
 			<div className="not-found">404</div>
+			{message && <p className="error-message">{message}</p>}
+
 			<style jsx>{`
 				.not-found {
 					color: #fff;
@@ -21,7 +30,23 @@ export default function Custom404() {
 				.not-found {
 					text-align: center;
 				}
+
+				.error-message {
+					color: #fff;
+					font-size: 2em;
+					font-weight: bold;
+					font-family: Helvetica;
+					margin-top: 10px;
+					text-shadow: 0 1px 0 #ccc, 0 2px 0 #c9c9c9, 0 3px 0 #bbb,
+						0 4px 0 #b9b9b9, 0 5px 0 #aaa,
+						0 6px 1px rgba(0, 0, 0, 0.1), 0 0 5px rgba(0, 0, 0, 0.1),
+						0 1px 3px rgba(0, 0, 0, 0.3), 0 3px 5px rgba(0, 0, 0, 0.2),
+						0 5px 10px rgba(0, 0, 0, 0.25),
+						0 10px 10px rgba(0, 0, 0, 0.2),
+						0 20px 20px rgba(0, 0, 0, 0.15);
+					text-align: center;
+				}
 			`}</style>
-		</>
+		</div>
 	)
 }
