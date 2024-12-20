dacb5d594231603b827ffe21a7b664a337fdcaec
Javier Madariaga <145422138+ldkjvvier@users.noreply.github.com>
2024-12-20 02:49:07 -0300

    feat: Refactor Custom404 component to use external CSS module for styling

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,4 @@
-'use client'
-
-import { Typography } from '@mui/material'
+import styles from './not-found.module.css'
 
 export default function Custom404({
 	message = '',
@@ -9,44 +7,10 @@ export default function Custom404({
 }) {
 	return (
 		<div>
-			<div className="not-found">404</div>
-			{message && <p className="error-message">{message}</p>}
-
-			<style jsx>{`
-				.not-found {
-					color: #fff;
-					font-size: 12em;
-					font-weight: bold;
-					font-family: Helvetica;
-					text-shadow: 0 1px 0 #ccc, 0 2px 0 #c9c9c9, 0 3px 0 #bbb,
-						0 4px 0 #b9b9b9, 0 5px 0 #aaa,
-						0 6px 1px rgba(0, 0, 0, 0.1), 0 0 5px rgba(0, 0, 0, 0.1),
-						0 1px 3px rgba(0, 0, 0, 0.3), 0 3px 5px rgba(0, 0, 0, 0.2),
-						0 5px 10px rgba(0, 0, 0, 0.25),
-						0 10px 10px rgba(0, 0, 0, 0.2),
-						0 20px 20px rgba(0, 0, 0, 0.15);
-				}
-
-				.not-found {
-					text-align: center;
-				}
-
-				.error-message {
-					color: #fff;
-					font-size: 2em;
-					font-weight: bold;
-					font-family: Helvetica;
-					margin-top: 10px;
-					text-shadow: 0 1px 0 #ccc, 0 2px 0 #c9c9c9, 0 3px 0 #bbb,
-						0 4px 0 #b9b9b9, 0 5px 0 #aaa,
-						0 6px 1px rgba(0, 0, 0, 0.1), 0 0 5px rgba(0, 0, 0, 0.1),
-						0 1px 3px rgba(0, 0, 0, 0.3), 0 3px 5px rgba(0, 0, 0, 0.2),
-						0 5px 10px rgba(0, 0, 0, 0.25),
-						0 10px 10px rgba(0, 0, 0, 0.2),
-						0 20px 20px rgba(0, 0, 0, 0.15);
-					text-align: center;
-				}
-			`}</style>
+			<div className={styles['not-found']}>404</div>
+			{message && (
+				<p className={styles['error-message']}>{message}</p>
+			)}
 		</div>
 	)
 }
