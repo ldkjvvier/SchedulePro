7b8403156dd58418bf87ec80c3562bebf7f62c14
Javier Madariaga <145422138+ldkjvvier@users.noreply.github.com>
2024-12-19 22:28:11 -0300

    refactor: Move mock data for barbers and services to separate data files

--- src/services/barber.ts
+++ src/services/barber.ts
@@ -1,4 +1,5 @@
 import { Barber } from '@/models/barber'
+import { mockBarber } from '@/data/barber'
 
 /* import apiClient from './apiClient'
 
@@ -7,52 +8,6 @@ export const getUser = async (userId: string) => {
 	return response.data
 }
  */
-const mockBarber: Barber[] = [
-	{
-		id: '1',
-		name: 'Havuer Barber',
-		email: 'john.barber@example.com',
-		image: '/user.png', // URL de imagen de perfil
-		specialties: [
-			'Cortes clásicos',
-			'Barbas estilo moderno',
-			'Cortes fade',
-		], // Especialidades
-		availability: {
-			monday: '9:00 AM - 5:00 PM',
-			tuesday: '9:00 AM - 5:00 PM',
-			wednesday: 'Cerrado',
-			thursday: '9:00 AM - 5:00 PM',
-			friday: '9:00 AM - 6:00 PM',
-			saturday: '10:00 AM - 4:00 PM',
-			sunday: 'Cerrado',
-		}, // Disponibilidad por día
-		bio: 'John es un barbero con más de 10 años de experiencia, especializado en cortes clásicos y tendencias modernas. Con una pasión por la precisión, asegura que cada cliente salga satisfecho.',
-		contactNumber: '+1 (555) 123-4567', // Teléfono de contacto
-	},
-	{
-		id: '2',
-		name: 'John Barber',
-		email: 'john.barber@example.com',
-		image: '/user.png', // URL de imagen de perfil
-		specialties: [
-			'Cortes clásicos',
-			'Barbas estilo moderno',
-			'Cortes fade',
-		], // Especialidades
-		availability: {
-			monday: '9:00 AM - 5:00 PM',
-			tuesday: '9:00 AM - 5:00 PM',
-			wednesday: 'Cerrado',
-			thursday: '9:00 AM - 5:00 PM',
-			friday: '9:00 AM - 6:00 PM',
-			saturday: '10:00 AM - 4:00 PM',
-			sunday: 'Cerrado',
-		}, // Disponibilidad por día
-		bio: 'John es un barbero con más de 10 años de experiencia, especializado en cortes clásicos y tendencias modernas. Con una pasión por la precisión, asegura que cada cliente salga satisfecho.',
-		contactNumber: '+1 (555) 123-4567', // Teléfono de contacto
-	},
-]
 
 export const fetchBarber = async (
 	barberId: string
