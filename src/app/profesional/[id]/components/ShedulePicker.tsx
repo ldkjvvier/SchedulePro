f1f7383b86f9be8ea1663da38d3164645bd72483
Javier Madariaga <145422138+ldkjvvier@users.noreply.github.com>
2024-12-30 16:21:35 -0300

    feat: Enhance SchedulePicker with improved date handling and UI layout

--- src/app/profesional/[id]/components/ShedulePicker.tsx
+++ src/app/profesional/[id]/components/ShedulePicker.tsx
@@ -1,33 +1,31 @@
-import { useEffect, useState } from 'react'
+import { useState, useEffect } from 'react'
 import {
 	Typography,
-	Button,
 	List,
 	ListItem,
 	ListItemButton,
+	Box,
 } from '@mui/material'
 import { useBarberShedule } from '@/hooks/useBarberShedule'
 
-export const SchedulePicker = () => {
-	const [selectedDate, setSelectedDate] = useState<string>(
-		new Date().toISOString().split('T')[0]
-	)
-	const [dates, setDates] = useState<string[]>([])
+export const SchedulePicker: React.FC = () => {
+	const [selectedDate, setSelectedDate] = useState<Date>(new Date())
+	const [dates, setDates] = useState<Date[]>([])
 	const {
 		data: schedule,
 		isLoading,
 		error,
 		refetch,
-	} = useBarberShedule('1', selectedDate)
+	} = useBarberShedule('1', selectedDate.toISOString().split('T')[0])
 
 	useEffect(() => {
-		const generateDates = () => {
+		const generateDates = (): void => {
 			const today = new Date()
-			const datesArray = []
+			const datesArray: Date[] = []
 			for (let i = 0; i < 7; i++) {
 				const date = new Date(today)
 				date.setDate(today.getDate() + i)
-				datesArray.push(date.toISOString().split('T')[0])
+				datesArray.push(date)
 			}
 			setDates(datesArray)
 		}
@@ -38,44 +36,70 @@ export const SchedulePicker = () => {
 		refetch()
 	}, [selectedDate, refetch])
 
-	const handleSelectDate = (date: string) => {
+	const handleSelectDate = (date: Date): void => {
 		setSelectedDate(date)
 	}
 
 	return (
-		<div className="tw-bg-white tw-w-full tw-h-full tw-text-black">
-			<Typography variant="subtitle1" gutterBottom>
-				Selecciona una fecha
-			</Typography>
-			<List>
-				{dates.map((date) => (
-					<ListItem key={date}>
-						<ListItemButton onClick={() => handleSelectDate(date)}>
-							{date}
-						</ListItemButton>
-					</ListItem>
-				))}
-			</List>
-			{selectedDate && (
-				<div>
+		<div className="tw-flex tw-items-center tw-justify-center tw-bg-secondary tw-w-full tw-h-full tw-text-black">
+			<Box className="tw-flex tw-items-center tw-justify-center tw-gap-4 tw-w-[80%] tw-h-[80%]">
+				<Box className="tw-w-full tw-h-full tw-rounded-md tw-bg-primary tw-shadow-md tw-p-4">
 					<Typography variant="subtitle1" gutterBottom>
-						Horarios disponibles para {selectedDate}:
+						Selecciona una fecha
 					</Typography>
-					{isLoading && <p>Cargando horarios...</p>}
-					{error && (
-						<p>Error al cargar los horarios: {error.message}</p>
+					<List>
+						{dates.map((date) => (
+							<ListItem key={date.toISOString()}>
+								<ListItemButton
+									onClick={() => handleSelectDate(date)}
+								>
+									{date.toDateString()}
+								</ListItemButton>
+							</ListItem>
+						))}
+					</List>
+					{selectedDate && (
+						<div>
+							<Typography variant="subtitle1" gutterBottom>
+								Horarios disponibles para{' '}
+								{selectedDate.toDateString()}:
+							</Typography>
+							{isLoading && <p>Cargando horarios...</p>}
+							{error && (
+								<p>Error al cargar los horarios: {error.message}</p>
+							)}
+							{schedule && (
+								<List>
+									{schedule.times.map((time) => (
+										<ListItem key={time}>
+											<ListItemButton>{time}</ListItemButton>
+										</ListItem>
+									))}
+								</List>
+							)}
+						</div>
 					)}
-					{schedule && (
-						<List>
-							{schedule.times.map((time) => (
-								<ListItem key={time}>
-									<ListItemButton>{time}</ListItemButton>
-								</ListItem>
-							))}
-						</List>
-					)}
-				</div>
-			)}
+				</Box>
+				<Box className="tw-w-[40%] tw-h-full tw-rounded-md tw-bg-primary tw-shadow-md tw-p-4">
+					<Typography variant="subtitle1" gutterBottom>
+						Información del servicio
+					</Typography>
+					<Box sx={{ p: 2 }} width={'100%'}>
+						<Typography variant="subtitle1" gutterBottom>
+							Servicio seleccionado
+						</Typography>
+						<Typography variant="body1" gutterBottom>
+							Nombre del servicio
+						</Typography>
+						<Typography variant="body1" gutterBottom>
+							Duración del servicio
+						</Typography>
+						<Typography variant="body1" gutterBottom>
+							Precio del servicio
+						</Typography>
+					</Box>
+				</Box>
+			</Box>
 		</div>
 	)
 }
