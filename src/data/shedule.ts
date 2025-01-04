d7c0647c76801f87a5e66bd71e45d5a1559e572c
Javier Madariaga <145422138+ldkjvvier@users.noreply.github.com>
2025-01-04 00:27:27 -0300

    feat: Refactor mockSchedule to include booking status and structured time slots

--- src/data/shedule.ts
+++ src/data/shedule.ts
@@ -1,30 +1,82 @@
-// src/data/schedule.ts
-
 import { Schedule } from '@/models/barber'
 
 export const mockSchedule: { [barberId: string]: Schedule[] } = {
 	'1': [
 		{
-			date: new Date().toISOString().split('T')[0],
-			times: ['9:00 AM', '10:00 AM', '11:00 AM'],
+			date: new Date().toISOString().split('T')[0], // Fecha de hoy
+			times: [
+				{
+					id: '1-1-1',
+					time: '09:00',
+					isBooked: false,
+				},
+				{
+					id: '1-1-2',
+					time: '10:00',
+					isBooked: false,
+				},
+				{
+					id: '1-1-3',
+					time: '11:00',
+					isBooked: true,
+				},
+			],
 		},
 		{
 			date: new Date(new Date().setDate(new Date().getDate() + 1))
 				.toISOString()
-				.split('T')[0],
-			times: ['1:00 PM', '2:00 PM'],
+				.split('T')[0], // Fecha de mañana
+			times: [
+				{
+					id: '1-2-1',
+					time: '13:00',
+					isBooked: false,
+				},
+				{
+					id: '1-2-2',
+					time: '14:00',
+					isBooked: true,
+				},
+			],
 		},
 	],
 	'2': [
 		{
-			date: new Date().toISOString().split('T')[0],
-			times: ['10:30 AM', '12:00 PM'],
+			date: new Date().toISOString().split('T')[0], // Fecha de hoy
+			times: [
+				{
+					id: '2-1-1',
+					time: '10:30',
+					isBooked: false,
+				},
+				{
+					id: '2-1-2',
+					time: '12:00',
+					isBooked: false,
+				},
+			],
 		},
 		{
 			date: new Date(new Date().setDate(new Date().getDate() + 1))
 				.toISOString()
-				.split('T')[0],
-			times: ['3:00 PM', '4:00 PM', '5:00 PM'],
+				.split('T')[0], // Fecha de mañana
+			times: [
+				{
+					id: '2-2-1',
+					time: '15:00',
+					isBooked: true,
+				},
+				{
+					id: '2-2-2',
+					time: '16:00',
+					isBooked: false,
+				},
+				{
+					id: '2-2-3',
+					time: '17:00',
+					isBooked: false,
+				},
+			],
 		},
 	],
 }
