de83f440e99b77fbddec311238506a0992a79e6e
Javier Madariaga <145422138+ldkjvvier@users.noreply.github.com>
2025-01-05 03:25:53 -0300

    refactor: Update store configuration to use shoppingCartSlice

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
-import cartReducer from './features/cartSlice'
+import cartReducer from './features/shoppingCartSlice'
 
 export const store = configureStore({
 	reducer: {
-		cart: cartReducer,
+		shoppingCart: cartReducer,
 	},
 })
 
