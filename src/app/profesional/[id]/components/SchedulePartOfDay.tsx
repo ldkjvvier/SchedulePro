76bc111749f40f6101505adf3090d7e06202b9b6
Javier Madariaga <145422138+ldkjvvier@users.noreply.github.com>
2025-01-04 23:05:03 -0300

    refactor: Remove ScheduleDateButton component and update SchedulePartOfDay for improved rendering

--- src/app/profesional/[id]/components/SchedulePartOfDay.tsx
+++ src/app/profesional/[id]/components/SchedulePartOfDay.tsx
@@ -21,16 +21,52 @@ export const SchedulePartOfDay = ({
 	const times = schedule.times.filter(filterFn)
 
 	if (times.length === 0) return null
+
 	return (
 		<Box>
 			<Typography variant="body1" gutterBottom>
 				{partOfDay}
 			</Typography>
 			<hr />
-			<List>
+			<List
+				sx={{
+					display: 'flex',
+					flexWrap: 'wrap',
+					gap: 2,
+					padding: 0,
+					margin: 2,
+				}}
+			>
 				{times.map((time) => (
-					<ListItem key={time.id}>
-						<ListItemButton>{time.time}</ListItemButton>
+					<ListItem
+						key={time.id}
+						sx={{
+							width: 'auto',
+							padding: 0,
+						}}
+					>
+						<ListItemButton
+							disabled={time.isBooked}
+							sx={{
+								minWidth: '80px', // Asegura que todos los botones tengan el mismo tamaño mínimo
+								textAlign: 'center',
+								justifyContent: 'center',
+								padding: '8px 16px',
+								backgroundColor: time.isBooked
+									? 'lightgray'
+									: 'white',
+								border: '1px solid',
+								borderRadius: '5px',
+								borderColor: time.isBooked ? 'gray' : 'primary.main',
+								'&:hover': {
+									backgroundColor: time.isBooked
+										? 'lightgray'
+										: 'primary.light',
+								},
+							}}
+						>
+							{time.time}
+						</ListItemButton>
 					</ListItem>
 				))}
 			</List>
