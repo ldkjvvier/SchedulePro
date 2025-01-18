025a475e33a31431a59a65740e91834219c3c832
Javier Madariaga <145422138+ldkjvvier@users.noreply.github.com>
2025-01-17 23:19:39 -0300

    feat: Refactor Header component to utilize HeaderBase for improved structure and readability

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,7 +1,5 @@
 'use client'
-import React, { useState } from 'react'
-import AppBar from '@mui/material/AppBar'
-import Toolbar from '@mui/material/Toolbar'
+import { useState } from 'react'
 import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button'
 import Link from 'next/link'
@@ -10,6 +8,7 @@ import MenuIcon from '@mui/icons-material/Menu'
 import CloseIcon from '@mui/icons-material/Close'
 import Drawer from '@mui/material/Drawer'
 import Image from 'next/image'
+import HeaderBase from './HeaderBase'
 
 const Header = () => {
 	const [isDrawerOpen, setIsDrawerOpen] = useState(false)
@@ -33,65 +32,40 @@ const Header = () => {
 		setIsDrawerOpen(!isDrawerOpen)
 	}
 
-	const Links = () => {
-		return (
-			<>
+	return (
+		<HeaderBase>
+			<Link className="tw-flex tw-items-center" href="/">
+				<Image
+					src="/page_logo.png"
+					alt="Barbería Logo"
+					width={40}
+					height={40}
+				/>
+				<Typography
+					variant="h6"
+					className="tw-ml-3 tw-text-text-main tw-text-xl tw-font-semibold"
+				>
+					SchedulePro
+				</Typography>
+			</Link>
+
+			<div className="tw-flex lg:tw-hidden">
+				<IconButton
+					onClick={toggleDrawer}
+					className="tw-text-text-main"
+				>
+					{isDrawerOpen ? <CloseIcon /> : <MenuIcon />}
+				</IconButton>
+			</div>
+
+			<nav className="tw-hidden lg:tw-flex tw-gap-4">
 				{navLinks.map((link) => (
 					<Link key={link.url} href={link.url} passHref>
-						<Button
-							className="tw-text-text-main hover:tw-bg-secondary hover:tw-text-text-main tw-rounded-lg tw-px-4"
-							variant="text"
-						>
-							{link.title}
-						</Button>
+						<Button variant="text">{link.title}</Button>
 					</Link>
 				))}
-			</>
-		)
-	}
+			</nav>
 
-	return (
-		<AppBar
-			position="static"
-			sx={{
-				backgroundColor: '#FFFFFF',
-			}}
-			elevation={0}
-		>
-			<Toolbar className="tw-justify-between">
-				{/* Logo y Nombre */}
-				<Link className="tw-flex tw-items-center" href="/">
-					<Image
-						src="/page_logo.png"
-						alt="Barbería Logo"
-						width={40}
-						height={40}
-					/>
-					<Typography
-						variant="h6"
-						className="tw-ml-3 tw-text-text-main tw-text-xl tw-font-semibold"
-					>
-						SchedulePro
-					</Typography>
-				</Link>
-
-				{/* Botón de Menú en dispositivos pequeños */}
-				<div className="tw-flex lg:tw-hidden">
-					<IconButton
-						onClick={toggleDrawer}
-						className="tw-text-text-main"
-					>
-						{isDrawerOpen ? <CloseIcon /> : <MenuIcon />}
-					</IconButton>
-				</div>
-
-				{/* Navegación en pantallas grandes */}
-				<nav className="tw-hidden lg:tw-flex tw-gap-4">
-					<Links />
-				</nav>
-			</Toolbar>
-
-			{/* Drawer para navegación en pantallas pequeñas */}
 			<Drawer
 				anchor="right"
 				open={isDrawerOpen}
@@ -104,12 +78,14 @@ const Header = () => {
 					>
 						Menú
 					</Typography>
-					<nav className="tw-flex tw-flex-col tw-gap-4">
-						<Links />
-					</nav>
+					{navLinks.map((link) => (
+						<Link key={link.url} href={link.url} passHref>
+							<Button variant="text">{link.title}</Button>
+						</Link>
+					))}
 				</div>
 			</Drawer>
-		</AppBar>
+		</HeaderBase>
 	)
 }
 
